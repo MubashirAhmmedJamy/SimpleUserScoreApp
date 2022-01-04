aa8d3002e12ab2f526d6e47f3aa06f2f944c2d37
MubashirAhmmedJamy <mubashirjamy@gmail.com>
2022-01-05 01:15:10 +0600

    Project initiated

---
+++ src/main/java/com/riseuplabs/SimpleUserScoreApp/SimpleUserScoreAppApplication.java
@@ -0,0 +1,12 @@
+package com.riseuplabs.SimpleUserScoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserScoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserScoreAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/riseuplabs/SimpleUserScoreApp/SimpleUserScoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riseuplabs.SimpleUserScoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserScoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
