d1d8962abc0e942444e2ffd872e89a7a9cee9b58
MubashirAhmmedJamy <mubashirjamy@gmail.com>
2022-01-06 14:54:43 +0600

    Mappings Enhanced

--- src/main/java/com/riseuplabs/SimpleUserScoreApp/utils/Mappings.java
+++ src/main/java/com/riseuplabs/SimpleUserScoreApp/utils/Mappings.java
@@ -5,8 +5,8 @@ public class Mappings {
 
     public static final String CREATE_USER = "/createUser";
     public static final String CREATE_USER_PROGRESS = "/createUserProgress";
-    public static final String GET_USER = "/getUser/{id}";
-    public static final String GET_USER_PROGRESS = "/getUserProgress/{id}";
+    public static final String GET_USER = "/users/{id}";
+    public static final String GET_USER_PROGRESS = "/users/{id}/getUserProgress";
     public static final String UPDATE_USER = "/updateUser";
     public static final String UPDATE_USER_PROGRESS = "/updateUserProgress";
 }
