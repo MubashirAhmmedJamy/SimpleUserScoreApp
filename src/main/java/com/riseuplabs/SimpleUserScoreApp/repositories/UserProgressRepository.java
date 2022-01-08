22b55e445179e99b7b873de0651be0f35feddc4c
MubashirAhmmedJamy <mubashirjamy@gmail.com>
2022-01-08 09:54:09 +0600

    Improvement: Missing @Repository annotation included into UserProgressRepository | project was already running | Application Fully functional

--- src/main/java/com/riseuplabs/SimpleUserScoreApp/repositories/UserProgressRepository.java
+++ src/main/java/com/riseuplabs/SimpleUserScoreApp/repositories/UserProgressRepository.java
@@ -4,9 +4,11 @@ import com.riseuplabs.SimpleUserScoreApp.models.UserProgress;
 import com.riseuplabs.SimpleUserScoreApp.pojo.commons.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserProgressRepository extends JpaRepository<UserProgress, Long> {
     @Query(value = "select * from (SELECT user_id, RANK() OVER (ORDER BY score desc) as user_rank FROM score.user_progress) as t where user_rank <= 10", nativeQuery = true)
     List<Rank> findCurrentRanking();
